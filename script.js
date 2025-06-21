cc0911ee650e6833ccf65d20bdb089daa70efad8
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-06-21 16:22:08 +0530

    string and number exercise

---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("Hello pokemon");
+console.log('Pikachu!');
---
+++ string exercise/script.js
@@ -0,0 +1,7 @@
+console.log("hello");
+let s="rishi";
+let n;
+n=s[0].toUpperCase();
+// let x=s[1];
+n=n+s.slice(1,s.length);
+console.log(n);
