69d59c390beb8e8f198eee9db5cc94c463678d82
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-06-21 16:48:11 +0530

    completed number exercise

---
+++ number exercise/script.js
@@ -0,0 +1,29 @@
+const x=87;
+const y=18;
+let result;
+let display_string;
+
+//Addition
+result=x+y;
+display_string=`${x} + ${y} = ${result}`;
+console.log(display_string);
+
+//Substraction
+result=x-y;
+display_string=`${x} - ${y} = ${result}`;
+console.log(display_string);
+
+//Multiplication
+result=x*y;
+display_string=`${x} * ${y} = ${result}`;
+console.log(display_string);
+
+//Division
+result=x/y;
+display_string=`${x} / ${y} = ${result}`;
+console.log(display_string);
+
+//Remainder
+result=x%y;
+display_string=`${x} % ${y} = ${result}`;
+console.log(display_string);
