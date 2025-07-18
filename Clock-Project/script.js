34445ee002370d6096af35ddb592db9f98f42179
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-07-18 15:45:13 +0530

    completed stopwatch logic

--- Clock-Project/script.js
+++ Clock-Project/script.js
@@ -17,57 +17,65 @@ const hour = document.querySelector('.hour');
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
 const milisecond = document.querySelector('.milisecond');
+let stopwatch;
 let stopwatchStatus = false;
 let mili = 0;
 let h=0,m=0,s=0;
 function change(){
-    mili += 10;
-    if(mili === 1000){
-        mili = 0;
-        s++;
-    }
-    if(s === 60){
-        s = 0;
-        m++;
-    }
-    if(m === 60){
-        m = 0;
-        h++;
-    }
-    milisecond.textContent = mili;
-    second.textContent = s;
-    minute.textContent = m;
-    hour.textContent = h;
-}
-
-function stopwatch(){
-    if(stopwatchStatus){
-        setInterval(change,10);
-    }
-    else{
+    // if(stopwatchStatus){
+        mili += 10;
+        if(mili === 1000){
+            mili = 0;
+            s++;
+        }
+        if(s === 60){
+            s = 0;
+            m++;
+        }
+        if(m === 60){
+            m = 0;
+            h++;
+        }
         milisecond.textContent = mili;
         second.textContent = s;
         minute.textContent = m;
         hour.textContent = h;
-    }
+    // }
 }
+
 
 startStopwatch.addEventListener('click' , () => {
     startStopwatch.className = 'start-stopwatch hide';
     // resetStopwatch.className = 'reset-stopwatch';
     stopStopwatch.className = 'stop-stopwatch';
-    stopwatchStatus = true;
-    stopwatch();
+    resetStopwatch.className = 'reset-stopwatch hide';
+    // stopwatchStatus = true;
+    stopwatch = setInterval(change,10);
     
 });
 stopStopwatch.addEventListener('click',() => {
     startStopwatch.className = 'start-stopwatch';
+    resetStopwatch.className = 'reset-stopwatch';
+    startStopwatch.innerText = 'Resume';
     stopStopwatch.className = 'stop-stopwatch hide';
-    stopwatchStatus = false;
-    stopwatch();
-
+    // stopwatchStatus = false;
+    clearTimeout(stopwatch);
+    
 });
-
+resetStopwatch.addEventListener('click', () => {
+    resetStopwatch.className = 'reset-stopwatch hide';
+    startStopwatch.innerText = 'Start';
+    mili = 0;
+    s = 0;
+    m = 0;
+    // clearTimeout(stopwatch);
+    h = 0;
+    // stopwatchStatus = false;
+    milisecond.textContent = mili;
+    second.textContent = s;
+    minute.textContent = m;
+    hour.textContent = h;
+});
 
 
 // for timer
