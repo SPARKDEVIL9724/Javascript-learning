2be96540f38f7f41c9a153dfd22bdd88130316be
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2025-07-19 15:26:59 +0530

    completed calculator project

--- Calculator-Project/script.js
+++ Calculator-Project/script.js
@@ -3,8 +3,9 @@ const buttonDiv = document.querySelector('.buttons');
 const calculatorInput = document.querySelector('.calculator-input');
 const form = document.querySelector('.calculator-display');
 let result = 0;
-let operator;
+let operator = '';
 let number = 0;
+let i = 0;
 
 // operation
 function operation(opp, numb, res){
@@ -13,16 +14,17 @@ function operation(opp, numb, res){
             res += numb;
             break;
         case '-':
-            res += numb;
+            res -= numb;
             break;
         case '*':
-            res += numb;
+            res *= numb;
             break;
         case '/':
             res /= numb;
             break;
         default:
     }
+    localStorage.setItem('result', res);
     return res;
 }
 
@@ -41,45 +43,87 @@ header.appendChild(h1);
 containerDiv.insertAdjacentElement('afterbegin', header);
 
 // creating buttons
-[1,2,3,4,5,6,7,8,9,0,'.','BackSpace','+','-','*','/','=','C'].forEach(i => createButton(i));
+[1,2,3,'Clear',4,5,6,'BackSpace',7,8,9,'=','+',0,'.','empty','-','*','/'].forEach(i => createButton(i));
 
 // events
 const num = ['1','2','3','4','5','6','7','8','9','0'];
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     if(num.includes(button.innerText)){
-        // localStorage.setItem('test', element.innerText);
         button.addEventListener('click', (e) => {
-            // const head = document.querySelector('h1');
-            number = number*10 + Number(e.target.innerText);
+            if(typeof(number) === 'string' && number.includes('.')){
+                number += e.target.innerText;
+            }
+            else{
+                number = Number(number);
+                number = number*10 + Number(e.target.innerText);
+            }
+            if(operator === ''){
+                result = number;
+            }
+            calculatorInput.value = number;
         });
     }
     else if(['+', '-', '*', '/'].includes(button.innerText)){
         button.addEventListener('click', (e) => {
             if(operator !== ''){
+                number = Number(number);
+                result = Number(result);
                 result = operation(operator, number, result);
-                number = 0;
+                calculatorInput.value = result;
                 operator = '';
             }
-            operator = button.innerText;
+            operator = e.target.innerText;
+            number = 0;
         });
     }
-    else if(button.innerText === 'C'){
+    else if(button.innerText === 'Clear'){
         button.addEventListener('click', (e) => {
             result = 0;
+            number = 0;
+            calculatorInput.value = result;
         });
     }
-    
-
+    else if(button.innerText === 'BackSpace'){
+        button.addEventListener('click', (e) => {
+            if(typeof(number) === 'string' && number.includes('.')){
+                number = number.slice(0, number.length-1);
+            }
+            else{
+                number = Number(number);
+                number = Math.floor(number/10);
+            }
+            calculatorInput.value = number;
+        });
+    }
+    else if(button.innerText === '.'){
+        button.addEventListener('click', (e) => {
+            number = number.toString();
+            if (number.includes('.')){
+            }
+            else{number += '.';}
+            calculatorInput.value = number ;
+            i=0;
+        });
+    }
+    else if(button.innerText === '='){
+        button.addEventListener('click', () => {
+            result = operation(operator, number, result);
+            calculatorInput.value = result;
+        });
+    }
 });
-// window.addEventListener('keydown', (e) => {
-//     if(e.key === 'Enter' || e.key === '='){
-//         calculatorDisplay.value = result;
-//     }
-//     else if(num.includes(e.key)){
-//         e.preventDefault();
-//         const formData = new FormData(form);
-//         const num = formData.set('item',e.key);
-//     }
-//     else{}
-// });
+calculatorInput.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    buttons.forEach((button) => {
+        if(e.key === button.innerText){
+            button.click();
+        }
+        else if(button.innerText === '=' && e.key === 'Enter'){
+            button.click();
+        }
+        else if(button.innerText === 'BackSpace' && e.keyCode === 8){
+            button.click();
+        }
+    });
+});
